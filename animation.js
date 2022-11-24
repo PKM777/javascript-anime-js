76be66a0b5065402fe8ac643dd3873bc0244739b
Praveen K M <99317905+PKM777@users.noreply.github.com>
2022-11-24 22:11:06 +0530

    animation using anime js

---
+++ animation.js
@@ -0,0 +1,23 @@
+const contain = document.querySelector(".container");
+
+for (let i = 0; i < 100; i++) {
+  const createB = document.createElement("div");
+  createB.classList.add("blocks");
+  contain.appendChild(createB);
+}
+
+function generate() {
+  anime({
+    targets: ".blocks",
+    translateX: function () {
+      return anime.random(-700, 700);
+    },
+    translateY: function () {
+      return anime.random(-700, 700);
+    },
+    scale: function () {
+      return anime.random(1, 6);
+    },
+  });
+  //   window.requestAnimationFrame(generate);
+}
